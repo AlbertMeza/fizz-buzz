927f168386d94fb62c020d5d93ce8fa584b5bd06
AlbertMeza <almez5202@gmail.com>
2022-09-20 11:03:30 -0700

    FizzBuzzTest started

--- src/test/java/com/tlglearning/FizzBuzzTest.java
+++ src/test/java/com/tlglearning/FizzBuzzTest.java
@@ -2,11 +2,14 @@ package com.tlglearning;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
   @Test
   void valueOf() {
+    assert Set.of(FizzBuzz.FIZZ).equals(FizzBuzz.valueOf(6));
+    assertEquals(Set.of(FizzBuzz.FIZZ), FizzBuzz.valueOf(6));
   }
 }
